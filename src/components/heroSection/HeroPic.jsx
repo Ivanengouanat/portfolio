a4319cc3d8eb2c2f667da7e62662a8bfa85a1552
Ivan <ngouanativan@gmail.com>
2025-07-15 15:36:22 +0100

    15 juillet

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/image/profile.png"
+        src="src/assets/image/profile.png"
         alt="ngouanat ivan"
         className="max-h-[450px] w-auto"
       />
